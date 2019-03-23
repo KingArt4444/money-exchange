60f933c16a7d8d1c3b0cecc9c26e25409446c128
KingArt4444 <artem_korolev13@mail.ru>
2019-03-23 22:36:13 +0300

    add working code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,62 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var localcurr = currency;
+    var obj = {};
+    if(localcurr > 10000){
+         obj.error = "You are rich, my friend! We don't have so much coins for exchange";
+        return obj;
+    } else if(localcurr < 0){
+        return obj;
+    }else{
+        for(var i = 1;;i++){
+            if(localcurr >= 50){
+                obj.H = i;
+                localcurr -= 50;
+            }
+            else{
+                break;
+            }
+        }
+        
+        for(var i = 1;;i++){
+            if(localcurr >= 25){
+                obj.Q = i;
+                localcurr -= 25;
+            }
+            else{
+                break;
+            }
+        }
+        
+        for(var i = 1;;i++){
+            if(localcurr >= 10){
+                obj.D = i;
+                localcurr -= 10;
+            }
+            else{
+                break;
+            }
+        }
+        
+        for(var i = 1;;i++){
+            if(localcurr >= 5){
+                obj.N = i;
+                localcurr -= 5;
+            }
+            else{
+                break;
+            }
+        }
+        
+        for(var i = 1;;i++){
+            if(localcurr >= 1){
+                obj.P = i;
+                localcurr -= 1;
+            }
+            else{
+                break;
+            }
+        }
+        return obj;
+    }
 }
